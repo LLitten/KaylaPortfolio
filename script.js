e560e0548cda43bfef7997fddfb05ca800de72f8
Liza litten <lgl0004@mix.wvu.edu>
2024-04-12 20:54:29 -0400

    added images to ITST Comics
    added ITST comic panels with image carousels
    image carousels may need to be at the bottom

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-let slideIndex = 0;
-const slides = document.querySelectorAll('.carousel-slide img');
+document.addEventListener("DOMContentLoaded", function() {
+    const carousels = document.querySelectorAll('.carousel-container');
 
-function showSlide(index) {
-    if (index >= slides.length) {
-        slideIndex = 0;
-    } else if (index < 0) {
-        slideIndex = slides.length - 1;
-    }
+    carousels.forEach(function(carousel) {
+        let slideIndex = 0;
+        const slides = carousel.querySelectorAll('.carousel-slide img');
+        const prevButton = carousel.querySelector('.prev');
+        const nextButton = carousel.querySelector('.next');
 
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
+        function showSlide(index) {
+            if (index >= slides.length) {
+                slideIndex = 0;
+            } else if (index < 0) {
+                slideIndex = slides.length - 1;
+            }
 
-    slides[slideIndex].style.display = 'block';
-}
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = 'none';
+            }
 
-function moveSlide(n) {
-    slideIndex += n;
-    showSlide(slideIndex);
-}
+            slides[slideIndex].style.display = 'block';
+        }
 
-showSlide(slideIndex);
+        function moveSlide(n) {
+            slideIndex += n;
+            showSlide(slideIndex);
+        }
 
+        showSlide(slideIndex);
+
+        prevButton.addEventListener('click', function() {
+            moveSlide(-1);
+        });
+
+        nextButton.addEventListener('click', function() {
+            moveSlide(1);
+        });
+    });
+});
